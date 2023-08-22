f38207d06a5a6fcf2a3b8710a91dabc788928877
Jashhwanthk <jaswanthk0402.xworkz@gmail.com>
2023-08-22 12:15:53 +0530

    variablesdatatypes

---
+++ Add.java
@@ -0,0 +1,10 @@
+class Add{
+	public static int add(int a,int b);
+    public static void main(String args[]){
+		int a = 7;
+		int b = 8;
+	System.out.println(the addition is : "+add");
+	
+	}
+	
+}
