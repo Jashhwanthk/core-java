e09d66df17993e8052b7f866485ae1bf7c982d8d
Jashhwanthk <142865696+Jashhwanthk@users.noreply.github.com>
2023-08-23 11:00:21 +0530

    Add files via upload

---
+++ Speaker3executor.java
@@ -0,0 +1,21 @@
+class Speaker3executor{
+
+public static void main(String jbl[]){
+
+
+           Speaker3.onOroff();
+		   Speaker3.onOroff();
+		   Speaker3.onOroff();
+		   Speaker3.onOroff();
+		   Speaker3.onOroff();
+		   Speaker3.onOroff();
+		   Speaker3.onOroff();
+		   Speaker3.onOroff();
+		   Speaker3.onOroff();
+		   Speaker3.onOroff();
+
+
+
+}
+
+}
