21e9563e290cecc0813748c8cfe96ea38a3a7c04
Jashhwanthk <142865696+Jashhwanthk@users.noreply.github.com>
2023-08-23 11:02:24 +0530

    Update Speaker3.java

--- Speaker3.java
+++ Speaker3.java
@@ -1,11 +1,26 @@
-class Speaker2{
-public static void ma
+class Speaker3{
+   static boolean isConnected;
+   
+   public static boolean onOroff(){
+   System.out.println("ON or OFF invoked");
+    
+   if (isConnected == false){
+	   isConnected = true;
+	   System.out.println("The system is turned on.....enjoyyy");
+   }   
+   
+   else if(isConnected == true){
+	   isConnected = false;
+	   System.out.println("The Speaker is turned off");
+	   }
+    System.out.println("end of onOroff");
+	
+	
+	return isConnected;
 
 
 
-
-
-
+   }
 
 
 }
