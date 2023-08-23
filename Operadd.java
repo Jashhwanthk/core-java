3b396123f7c58faf5c597b64680df13c9f3c5e6c
Jashhwanthk <jaswanthk0402.xworkz@gmail.com>
2023-08-23 07:04:13 +0530

    addsubdivmulmod

---
+++ Operadd.java
@@ -0,0 +1,28 @@
+class Operadd {
+	    public static void main(String add[]){
+        
+		 add(7 , 10);
+		 add(7 , 1);
+		 add(7 , 77);
+		 add(89 , 10);
+		 add(72 , 11);
+		 add(87 , 111);
+		 add(55 , 10);
+		 add(84 , 87);
+		 add(71 , 19);
+		 add(76 , 90);
+		
+		}
+		
+		public static void add(int a,int b){
+			System.out.println(a+b);		
+		}
+		
+
+
+
+}
+
+
+
+
---
+++ Opermod.java
@@ -0,0 +1,24 @@
+class Opermod{
+	public static void main(String mod[]){
+		                mod(77,10);
+		                mod(45,8);
+		                mod(45,7);
+				        mod(46,9);
+						mod(88,3);
+						mod(5,3);
+						mod(56,6);
+						mod(77,65);
+						mod(23,9);
+						mod(32,6);	                
+			}
+			public static void mod(int a, int b){
+				System.out.println(a%b);
+			}
+	
+	
+	
+}
+//Synatx method -- <public static     void                          main
+ //                <access-modifier   returntype                    methodname()
+                                    //not expecting                 in() we write parameter,this is called as parametric variable
+									//primitive/nonprimitive//
