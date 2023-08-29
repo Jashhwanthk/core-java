17a5079931a58d45a24839e09216a6e676eeb400
Jashhwanthk <jaswanthk0402.xworkz@gmail.com>
2023-08-29 13:18:34 +0530

    uploaded

---
+++ Appartment.java
@@ -0,0 +1,24 @@
+class Appartment{
+	
+	 String appartmentName ;
+	 int appartmentId ;
+	 String location  ;
+	 int noOf2bhkflats;
+	 int noOf3bhkflats;
+	 int noOffloors ;
+
+	//new to create object in JAVA//
+	
+	
+        public void provideAccomodation(){
+		System.out.println("Method created for testing");
+		
+		
+	}
+	
+	
+	 
+	
+
+
+}
---
+++ Sensor.java
@@ -0,0 +1,17 @@
+class Sensors{
+	
+	int sensorId;
+	String sensorType;
+	String sensorSupply;
+	String sensorMount;
+	int sensorPrice;
+	
+	
+	
+	
+	public void sensorReaction(){
+		System.out.println("The sensor is reacts for action");
+	}
+	
+	
+}
