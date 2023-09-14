e3c8bea2769495c6fc6a64eb1f6459d474119432
Jashhwanthk <jaswanthk0402.xworkz@gmail.com>
2023-09-14 10:34:12 +0530

    crud

---
+++ OttPlatformExecutor.java
@@ -0,0 +1,15 @@
+class ottPlatformExecutor{
+
+	public static void main(String args []){
+	
+	ottPlatform movie = new ottPlatform();
+	 movie.addMovieName("om");
+	 movie.addMovieName("Jogi");
+	 movie.addMovieName("Ugrum");
+	 movie.addMovieName("Upendra");
+	 
+	  movie.getMovies();
+	
+	}
+
+}
